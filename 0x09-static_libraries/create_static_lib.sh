d0214bd050566ae0833156488c4d0b2e3d9c2f0c
Chioma Victor <chisquarevic@gmail.com>
2023-08-04 12:18:46 -0700

    static

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-gcc -wall -pedantic -werror -wextra -c *.c
-ar -rc liball.a *.o
-ranlib liball.a
+
+# compile all .c files in the current directory into object files
+gcc -c *.c
+
+# create the static library liball.a from all the generated objects files
+ar rcs liball.a *.o
+
+# clean up by removing the object files
+rm -f *.o
